818c8743a5a1ef2e0fdd78acf115ba102000b429
ModischFabrications <modisch.fabrications@gmail.com>
2019-07-02 13:05:21 +0200

    extract

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from PySide2.QtWidgets import QApplication
 
 
 def main():
+    # could accept args, no idea why
     app = QApplication()
 
-    # TODO: use compiled ui file instead of dynamic loading
-    #  pyside2-uic mainwindow.ui > ui_mainwindow.py
-    ui_file = QFile("mainwindow.ui")
-    ui_file.open(QFile.ReadOnly)
-
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
+    window = get_ui()
     window.show()
 
     exit(app.exec_())
+
+
+def get_ui():
+    # TODO: use compiled ui file instead of dynamic loading
+    #  pyside2-uic mainwindow.ui > ui_mainwindow.py
+
+    ui_file = QFile("mainwindow.ui")
+    ui_file.open(QFile.ReadOnly)
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    return window
 
 
 if __name__ == '__main__':
