1b9dcb073854592d911527c47db683054baa118f
ModischFabrications <modisch.fabrications@gmail.com>
2019-07-02 12:57:45 +0200

    init with first example ui

---
+++ main.py
@@ -0,0 +1,22 @@
+from PySide2.QtCore import QFile
+from PySide2.QtWidgets import QApplication, QLabel
+from PySide2.QtUiTools import QUiLoader
+
+
+def main():
+    app = QApplication()
+
+    # TODO: use compiled ui file instead of dynamic loading
+    #  pyside2-uic mainwindow.ui > ui_mainwindow.py
+    ui_file = QFile("mainwindow.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    window.show()
+
+    exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
